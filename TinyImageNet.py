54a8dded002bed3a25324df9e1c5a231618dd55c
Cloudwalker <pranav13phoenix@gmail.com>
2022-09-21 23:23:03 +0530

    Update TinyImageNet.py

--- TinyImageNet.py
+++ TinyImageNet.py
@@ -13,7 +13,7 @@ from torchvision.io import read_image, ImageReadMode
 torch.backends.cudnn.benchmarks = True
 torch.backends.cudnn.deterministic = True
 
-batch_size = 64
+
 
 id_dict = {}
 for i, line in enumerate(open('/content/tiny-imagenet-200/wnids.txt', 'r')):
@@ -65,6 +65,8 @@ class TestTinyImageNetDataset(Dataset):
 
 transform_train = transforms.Normalize((122.4598, 114.2580, 101.3656), (70.4909, 68.5596, 71.8058))
 transform_test = transforms.Normalize((122.9611, 114.5793, 101.4786), (70.4953, 68.6092, 71.9997))
+
+batch_size = 64
 
 trainset = TrainTinyImageNetDataset(id=id_dict, transform = transform_train)
 trainloader = torch.utils.data.DataLoader(trainset, batch_size=batch_size, shuffle=True, num_workers=2, pin_memory=True, prefetch_factor=2, persistent_workers=2)
