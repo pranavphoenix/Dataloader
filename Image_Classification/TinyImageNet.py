223b8757c3c890b08ca8ff2b573d8fd59ca0a9c8
Cloudwalker <pranav13phoenix@gmail.com>
2023-05-04 06:29:56 +0530

    Update TinyImageNet.py

--- Image_Classification/TinyImageNet.py
+++ Image_Classification/TinyImageNet.py
@@ -1,4 +1,5 @@
 #loads images as 3*64*64 tensors 
+from PIL import Image
 
 !wget http://cs231n.stanford.edu/tiny-imagenet-200.zip
 !unzip -q tiny-imagenet-200.zip
@@ -9,19 +10,18 @@ import torchvision.transforms as transforms
 from torch.utils.data import DataLoader
 from torch.utils.data import Dataset
 import os, glob
-from torchvision.io import read_image, ImageReadMode
 torch.backends.cudnn.benchmarks = True
 torch.backends.cudnn.deterministic = True
 
 
 
 id_dict = {}
-for i, line in enumerate(open('/content/tiny-imagenet-200/wnids.txt', 'r')):
+for i, line in enumerate(open('/workspace/tiny-imagenet-200/wnids.txt', 'r')):
   id_dict[line.replace('\n', '')] = i
 
 class TrainTinyImageNetDataset(Dataset):
     def __init__(self, id, transform=None):
-        self.filenames = glob.glob("/content/tiny-imagenet-200/train/*/*/*.JPEG")
+        self.filenames = glob.glob("/workspace/tiny-imagenet-200/train/*/*/*.JPEG")
         self.transform = transform
         self.id_dict = id
 
@@ -30,21 +30,22 @@ class TrainTinyImageNetDataset(Dataset):
 
     def __getitem__(self, idx):
         img_path = self.filenames[idx]
-        image = read_image(img_path)
-        if image.shape[0] == 1:
-          image = read_image(img_path,ImageReadMode.RGB)
+        image = Image.open(img_path)
+
+        if image.mode == "L":
+          image = image.convert('RGB')
         label = self.id_dict[img_path.split('/')[4]]
         if self.transform:
-            image = self.transform(image.type(torch.FloatTensor))
+            image = self.transform(image)
         return image, label
 
 class TestTinyImageNetDataset(Dataset):
     def __init__(self, id, transform=None):
-        self.filenames = glob.glob("/content/tiny-imagenet-200/val/images/*.JPEG")
+        self.filenames = glob.glob("/workspace/tiny-imagenet-200/val/images/*.JPEG")
         self.transform = transform
         self.id_dict = id
         self.cls_dic = {}
-        for i, line in enumerate(open('/content/tiny-imagenet-200/val/val_annotations.txt', 'r')):
+        for i, line in enumerate(open('/workspace/tiny-imagenet-200/val/val_annotations.txt', 'r')):
             a = line.split('\t')
             img, cls_id = a[0],a[1]
             self.cls_dic[img] = self.id_dict[cls_id]
@@ -55,16 +56,24 @@ class TestTinyImageNetDataset(Dataset):
 
     def __getitem__(self, idx):
         img_path = self.filenames[idx]
-        image = read_image(img_path)
-        if image.shape[0] == 1:
-          image = read_image(img_path,ImageReadMode.RGB)
+        image = Image.open(img_path)
+        if image.mode == "L":
+          image = image.convert('RGB')
         label = self.cls_dic[img_path.split('/')[-1]]
         if self.transform:
-            image = self.transform(image.type(torch.FloatTensor))
+            image = self.transform(image)
         return image, label
 
-transform_train = transforms.Normalize((122.4598, 114.2580, 101.3656), (70.4909, 68.5596, 71.8058))
-transform_test = transforms.Normalize((122.9611, 114.5793, 101.4786), (70.4953, 68.6092, 71.9997))
+
+transform_train = transforms.Compose(
+        [ 
+            transforms.ToTensor(),
+     transforms.Normalize((0.4803, 0.4481, 0.3975), (0.2770, 0.2691, 0.2821))])
+
+transform_test = transforms.Compose(
+        [ 
+            transforms.ToTensor(),
+     transforms.Normalize((0.4823, 0.4495, 0.3982), (0.2771, 0.2694, 0.2830))])
 
 batch_size = 64
 
